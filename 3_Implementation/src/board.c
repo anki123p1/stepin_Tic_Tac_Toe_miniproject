75aeadcb0dfb5ebb657b20547f80b374475b7f84
anki123p1 <101923433+anki123p1@users.noreply.github.com>
2022-03-31 16:27:21 +0530

    Create board.c

---
+++ 3_Implementation/src/board.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include <windows.h>
+#include"passwin.h"
+
+int board[10];
+int gotoxy();
+void draw_board()
+{
+    int j;
+
+    for(j=9; j<17; j++)
+    {
+        gotoxy(35,j);
+        printf("|       |");
+    }
+    gotoxy(28,11);
+    printf("-----------------------");
+    gotoxy(28,14);
+    printf("-----------------------");
+
+    for(j=1; j<10; j++)
+    {
+        if(board[j] == 3)
+            put_X_O('X',j);
+        else if(board[j] == 5)
+            put_X_O('O',j);
+    }
+}
+
+void put_X_O(char ch,int pos)
+{
+    int m;
+    int x = 31, y = 10;
+
+    m = pos;
+
+    if(m > 3)
+    {
+        while(m > 3)
+        {
+            y += 3;
+            m -= 3;
+        }
+    }
+    if(pos % 3 == 0)
+        x += 16;
+    else
+    {
+        pos %= 3;
+        pos--;
+        while(pos)
+        {
+            x+=8;
+            pos--;
+        }
+    }
+    gotoxy(x,y);
+    printf("%c",ch);
+}
